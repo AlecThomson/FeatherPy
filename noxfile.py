872a5aa81579795082015054171878d0b2d7a8ba
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2024-11-07 09:00:41 +0800

    ignore invalid

--- noxfile.py
+++ noxfile.py
@@ -42,6 +42,7 @@ def tests(session: nox.Session) -> None:
     """
     Run the unit and regular tests.
     """
+    session.env["PYTHONWARNINGS"] = "ignore:invalid escape sequence"
     session.install(".[test]")
     session.run("pytest", *session.posargs)
 
