2ee2c97c5a1d6b4ace2fe1ffe1050eb6b11943ec
Thomson, Alec (CASS, Kensington) <alec.thomson@csiro.au>
2024-11-18 17:52:37 +1100

    Rename

--- src/featherpy/core.py
+++ src/featherpy/core.py
@@ -16,7 +16,7 @@ from radio_beam import Beam
 from scipy import fft
 
 from featherpy.exceptions import ShapeError, UnitError
-from featherpy.logging import logger
+from FeatherPy.src.featherpy.logger import logger
 from featherpy.plotting import plot_feather
 from featherpy.weighting import ONE_STD, sigmoid
 
